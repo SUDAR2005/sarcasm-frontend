88c0c1a86333e11421182a40f61fdb16657c270e
SUDAR2005 <sudarsettaiyan@gmail.com>
2024-10-27 22:52:30 +0530

    Structurally Modified Commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+import React,{useState } from "react";
+import "./App.css";
+
+function App(){
+  const [headline,setHeadline]=useState("");
+  const [result,setResult]=useState("Enter Headline to display result.");
+  const [loading,setLoading]=useState(false);
+
+  const handleChange=(e) =>{
+    setHeadline(e.target.value);
+  };
+
+  const predictSarcasm=async () =>{
+    if (!headline){
+      setResult("Please enter a headline.");
+      return;
+    }
+
+    setLoading(true);
+    try{
+      const response=await fetch("http://127.0.0.1:5000/predict",{
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({text: headline }),
+      });
+
+      const result=await response.json();
+      setResult(result.is_sarcastic ? "This headline is sarcastic." : "This headline is not sarcastic.");
+    } catch (error){
+      setResult("Error: Unable to process your request.");
+    } finally{
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <div className="heading">
+        <h2>Check Your Sarcasm</h2>
+      </div>
+      <div className="app-container">
+        <h2>Sarcasm Detection</h2>
+        <div className="input-container">
+          <label htmlFor="headline">Enter a Headline</label>
+          <input
+            type="text"
+            id="headline"
+            value={headline}
+            onChange={handleChange}
+            placeholder="Type your headline here"
+            className="input-box"
+          />
+          <button onClick={predictSarcasm} className="detect-button">
+          {loading ? "Checking..." : "Check Sarcasm"}
+          </button>
+        </div>
+        <h2>Result</h2>
+        <p className="result">{result}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
