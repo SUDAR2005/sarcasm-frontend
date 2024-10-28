81a123deff75336240d8d542a8b50ded75c82b29
SUDAR2005 <sudarsettaiyan@gmail.com>
2024-10-28 07:54:58 +0530

    Updated with firebase

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App(){
 
     setLoading(true);
     try{
-      const response=await fetch("http://127.0.0.1:5000/predict",{
+      const response=await fetch("https://sarcasm-detection-backend.onrender.com/predict",{
         method: "POST",
         headers:{
           "Content-Type": "application/json",
